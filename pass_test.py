fdf317adb6f09dba636a603599229e635bd37bc6
Mercy Maloba <mercy.maloba@student.moringaschool.com>
2021-08-01 21:58:27 +0300

    add user object

--- pass_test.py
+++ pass_test.py
@@ -1,1 +1,12 @@
 import unittest
+from pass_locker import User
+class TestUser (unittest.TestCase):
+    """
+    Test class that defines test cases for the User class behaviours.
+    """
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_user = User("Malobz", "kjdi") #create user object
+        
